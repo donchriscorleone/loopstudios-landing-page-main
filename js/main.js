a32b1df7c53df3a9dfc9af21ae5cb6e19d8b52be
Christopher II Lajom <christopheriilajom@gmail.com>
2022-03-26 17:37:07 +0800

    Initialized.

---
+++ js/main.js
@@ -0,0 +1,35 @@
+let isOpen = true;
+const hamBtn = document.querySelector('button.icon-btn');
+const header = document.querySelector('header.header');
+const headerContent = document.querySelector('div.header-content');
+const headerList = document.querySelector('ul.header-list-popup')
+
+window.addEventListener('resize', (e) => {
+    if (window.innerWidth > 900) {
+        showMenu(false, false);
+    }
+})
+
+if (hamBtn) {
+    hamBtn.addEventListener('click', (e) => {
+        showMenu(isOpen, !isOpen);
+    })
+}
+
+function showMenu(toShow, newValue) {
+    if (!toShow) {
+        hamBtn.style.backgroundImage = 'url("./../images/icon-hamburger.svg")';
+        header.style.backgroundColor = "none";
+        header.style.backgroundImage = "url('./../images/mobile/image-hero.jpg')";
+        headerContent.style.display = "block";
+        headerList.style.display = "none";
+    } else {
+        header.style.backgroundColor = "black";
+        header.style.backgroundImage = "none";
+        hamBtn.style.backgroundImage = 'url("./../images/icon-close.svg")';
+        headerContent.style.display = "none";
+        headerList.style.display = "flex";
+    }
+
+    isOpen = newValue;
+}
